f1e76e75617d568ec2f04e4a60216676d7d1d9d6
Anirban <anirbanchondrobanai@gmail.com>
2023-09-12 10:31:58 +0600

    up

--- src/app/(alll)/dashboard/page.jsx
+++ src/app/(alll)/dashboard/page.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div>
-            dashboard
+        <div >
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati cum ex quasi soluta accusantium, eligendi, error reprehenderit quis, dolore id aspernatur tempore in. Doloribus dicta similique consequatur quae incidunt qui alias fugit laborum aliquid, exercitationem mollitia facere reiciendis optio a beatae, dolores eaque perspiciatis tempora temporibus ea omnis ipsa. Sint a id obcaecati soluta tempora.
         </div>
     );
 };
